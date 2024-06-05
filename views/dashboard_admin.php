9d634e09fc8cf21a86ab3886c9ccd542d6fb49eb
Caio Tomaz <126210948+caiotomaza@users.noreply.github.com>
2024-06-05 09:54:59 -0300

    Criação da estrutura de arquivos (MVC, DAO e Singleton)

---
+++ views/dashboard_admin.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eMenu</title>
+</head>
+<body>
+    <header name = 'Cabeçario'>
+        <nav name = 'Navega'>
+        </nav>
+    </header>
+
+    <aside name = '?'>
+    </aside>
+
+    <footer name = 'Rodape'>
+    </footer>
+</body>
+</html>

c2cc81734cf6fdbd00a3c0f8357b6b1a69beb7d7
Caio Tomaz <126210948+caiotomaza@users.noreply.github.com>
2024-06-05 10:06:14 -0300

    Criação das classes;

--- models/classes/cliente.php
+++ models/classes/cliente.php
@@ -1,3 +1,46 @@
 <?php
+require_once('usuario.php');
 
+class cliente extends usuario{
+    public function __construct($id, $nome, $email, $senha){
+        $this-> id = $id;
+        $this-> nome = $nome;
+        $this-> email = $email;
+        $this-> senha = $senha;
+        }
+    
+        //Getters
+        public function getId(){
+            return $this-> id;
+        }
+    
+        public function getNome(){
+            return $this-> nome;
+        }
+    
+        public function getEmail(){
+            return $this-> email;
+        }
+    
+        public function getSenha(){
+            return $this-> senha;
+        }
+    
+        //Setters
+        public function setId($id){
+            $this-> id = $id;
+        }
+    
+        public function setNome($nome){
+            $this-> nome = $nome;
+        }
+        
+        public function setEmail($email){
+            $this-> email = $email;
+        }
+        
+        public function setSenha($senha){
+            $this-> senha = $senha;
+        }
+}
 ?>
